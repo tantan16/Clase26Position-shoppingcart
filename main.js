566e8e1aa8f466cf8a36c797f4d3b21beafc755f
tantan16 <tania.elizalde.vargas@gmail.com>
2020-10-10 22:04:59 -0500

    position

---
+++ main.js
@@ -0,0 +1,11 @@
+const botonAbrirMenu = document.getElementById("abrir-menu")
+const menu = document.getElementById("menu")
+const botonCerrarMenu = document.getElementById("cerrar-menu")
+
+botonAbrirMenu.onclick = () => {
+    menu.classList.remove("ocultar")
+}
+
+botonCerrarMenu.onclick = () =>{
+    
+}
